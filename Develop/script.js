543dcc207b41ffa7062472c93ab4a1eb3775d710
Mary Cohan <mary.cohan@gmail.com>
2021-01-26 17:31:56 -0800

    setting up javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,42 @@
 $document.ready(function() {
 
-var time 
+var myDay 
+
+var now = moment()
+
+function getHeaderDate() {
+    var currentHeaderDate = moment().format('dddd, MMMM Do');
+    $("#currentDay").text(currentHeaderDate);
+}
+// loads header date
+getHeaderDate();
 
 
+
+
+
+// saves data to localStorage
+function saveReminders() {
+    localStorage.setItem("myDay", JSON.stringify(myDay));
+}
+
+// sets any data in localStorage to the view
+function displayReminders() {
+    // myDay.forEach(function (_thisHour) {
+    //     $(`#${_thisHour.id}`).val(_thisHour.reminder);
+    // })
+}
+
+// sets any existing localStorage data to the view if it exists
+function init() {
+    var storedDay = JSON.parse(localStorage.getItem("myDay"));
+
+    if (storedDay) {
+        myDay = storedDay;
+    }
+
+    saveReminders();
+    displayReminders();
+}
 
 })
